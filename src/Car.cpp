a06ca801f1b196350ccb860b9f2c781d8e597163
Miguel Vera <miguel.coimbra.vera@gmail.com>
2015-09-30 00:34:14 +0100

    car changes

--- src/Car.cpp
+++ src/Car.cpp
@@ -22,7 +22,7 @@ void Car::draw()
 	glPushMatrix();
 
 	 //cubo1
-	 glColor3f(1, 0, 0);
+	 glColor3f(0.33, 0.2, 0.15);
 	 glPushMatrix();
 	  glTranslatef(0, 0, 0);
 	  glScalef(14, 3, 8);
@@ -30,7 +30,7 @@ void Car::draw()
 	 glPopMatrix();
   
 	 //cubo2
-	 glColor3f(0, 0, 1);
+	 glColor3f(0.20, 0.21, 0.22);
 	 glPushMatrix();
 	  glTranslatef(-3.5, 3.5, 0);
 	  glScalef(7, 4, 8);
@@ -38,7 +38,7 @@ void Car::draw()
 	 glPopMatrix();
  
 	 //cubo3
-	 glColor3f(0, 0, 0);
+	 glColor3f(0.37, 0.34, 0-.33);
 	 glPushMatrix();
 	  glTranslatef(2.5, 2.5, 0);
 	  glScalef(5, 2, 4);
@@ -46,32 +46,32 @@ void Car::draw()
 	 glPopMatrix();
 
 	 //rodas frente
-	 glColor3f(0,  0.5, 0.5);
+	 glColor3f(0, 0, 0);
 	 glPushMatrix();
 	  glPushMatrix();
-	   glTranslatef(3, -1.5, -4.5);
+	   glTranslatef(4, -1.5, -4.5);
 	   glScalef(1.5,1.5,1.5);
-	   glutSolidTorus(0.5,1,12,15);
+	   glutSolidTorus(0.7,1,12,15);
 	  glPopMatrix();
 	  glPushMatrix();
-	   glTranslatef(3, -1.5, 4.5);
+	   glTranslatef(4, -1.5, 4.5);
 	   glScalef(1.5,1.5,1.5);
-	   glutSolidTorus(0.5,1,12,15);
+	   glutSolidTorus(0.7,1,12,15);
 	  glPopMatrix();
 	 glPopMatrix();
 
 	//rodas trás
-	 glColor3f(0, 1, 0);
+	 glColor3f(0, 0, 0);
 	 glPushMatrix();
 	  glPushMatrix();
 	   glTranslatef(-5.5, -0.75, -5);
 	   glScalef(2.25, 2.25, 2);
-	   glutSolidTorus(0.5, 1, 12, 15);
+	   glutSolidTorus(0.6, 1, 12, 15);
 	  glPopMatrix();
 	  glPushMatrix();
 	   glTranslatef(-5.5, -0.75, 5);
 	   glScalef(2.25, 2.25, 2);
-	   glutSolidTorus(0.5, 1, 12, 15);
+	   glutSolidTorus(0.6, 1, 12, 15);
 	  glPopMatrix();
 	 glPopMatrix();
 	glPopMatrix();
